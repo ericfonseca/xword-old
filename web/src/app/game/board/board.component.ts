fdbaad421db960c056d405273e29f74d423fc885
Victoria Do <victoriado@outlook.com>
2018-02-08 09:21:22 -0800

    Move game board into Game

--- web/src/app/game/board/board.component.ts
+++ web/src/app/game/board/board.component.ts
@@ -1,9 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Game } from '@app/models';
 
-type Tile = -1 | string;
-const BOARD_SIZE = 10;
-
 @Component({
   selector: 'xw-board',
   templateUrl: './board.component.html',
@@ -11,40 +8,12 @@ const BOARD_SIZE = 10;
 })
 export class BoardComponent implements OnInit {
   @Input() public game: Game;
-  public board: Tile[][];
 
   constructor() {
-    this.initBoard();
+
   }
 
   public ngOnInit() {
-    this.populateBoard();
-  }
 
-  private initBoard() {
-    this.board = [];
-    for (let i = 0; i < BOARD_SIZE; i++) {
-      const row = [];
-      for(let j = 0; j < BOARD_SIZE; j++) {
-        row.push(-1);
-      }
-      this.board.push(row);
-    }
   }
-
-  private populateBoard() {
-    const { crossword } = this.game;
-    crossword.clues.forEach((clue) => {
-      let [x, y] = [clue.position.x, clue.position.y];
-      for (let i = 0; i < clue.tileLength; i++) {
-        this.board[x][y] = '';
-        if (clue.direction === 'A') {
-          x++;
-        } else {
-          y++;
-        }
-      }
-    });
-  }
-
 }
--- web/src/app/models/game.ts
+++ web/src/app/models/game.ts
@@ -1,5 +1,8 @@
 import { Crossword } from './crossword';
 
+type Tile = -1 | string;
+const BOARD_SIZE = 10;
+
 interface GameParams {
   id: string;
   crossword: Crossword;
@@ -8,8 +11,38 @@ interface GameParams {
 export class Game {
   public id: string;
   public crossword: Crossword;
+  public board: Tile[][];
 
   constructor(args: GameParams) {
     Object.assign(this, args);
+
+    this.initBoard();
+  }
+
+  private initBoard() {
+    this.board = [];
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      const row = [];
+      for(let j = 0; j < BOARD_SIZE; j++) {
+        row.push(-1);
+      }
+      this.board.push(row);
+    }
+
+    this.populateBoard();
+  }
+
+  private populateBoard() {
+    this.crossword.clues.forEach((clue) => {
+      let [x, y] = [clue.position.x, clue.position.y];
+      for (let i = 0; i < clue.tileLength; i++) {
+        this.board[x][y] = '';
+        if (clue.direction === 'A') {
+          x++;
+        } else {
+          y++;
+        }
+      }
+    });
   }
 }
